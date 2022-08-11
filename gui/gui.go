68292864ed2621f770e824b836d7bded411c0b35
Kristian Mide <fas@mide.dk>
2022-08-11 09:45:47 +0200

    Added input query

--- gui/gui.go
+++ gui/gui.go
@@ -6,9 +6,11 @@ import (
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/io/key"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
@@ -17,23 +19,39 @@ import (
 	"gioui.org/font/gofont"
 )
 
-var header material.LabelStyle
 var th *material.Theme
+var mainColor color.NRGBA
+
+var header material.LabelStyle
+
 var searchInput = &widget.Editor{
 	SingleLine: true,
 	Submit:     true,
 }
-
-var mainColor color.NRGBA
+var editor layout.Widget
 
 func Run() {
 	// initialize colors and theme
 	mainColor = color.NRGBA{R: 127, G: 127, B: 87, A: 255}
 	th = material.NewTheme(gofont.Collection())
 
-	header = material.H3(th, "Password CyberCracker")
+	header = material.H3(th, "Cyber Cracker")
 	header.Color = mainColor
 	header.Alignment = text.Middle
+
+	q := material.Editor(th, searchInput, "Query...")
+	q.Color = mainColor
+	q.HintColor = mainColor
+	q.Font.Style = text.Italic
+
+	border := widget.Border{Color: mainColor, CornerRadius: unit.Dp(8), Width: unit.Dp(2)}
+	editor = func(gtx layout.Context) layout.Dimensions {
+		return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			return layout.UniformInset(unit.Dp(8)).Layout(gtx, q.Layout)
+		})
+	}
+
+	//searchInput.Focus()
 
 	go func() {
 		w := app.NewWindow()
@@ -50,31 +68,23 @@ func loop(w *app.Window) error {
 	for {
 		e := <-w.Events()
 		switch e := e.(type) {
+		case key.Event:
+			log.Printf("Event %+v", e)
+			if e.Modifiers.Contain(key.ModCtrl) {
+				if e.Name == "c" {
+					return nil
+				}
+			}
 		case system.DestroyEvent:
 			return e.Err
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
-			//paint.Fill(&ops, color.NRGBA{R: 0, G: 0, B: 0, A: 255})
+			paint.Fill(&ops, color.NRGBA{R: 0, G: 0, B: 0, A: 255})
 
 			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 				layout.Rigid(header.Layout),
-				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					e := material.Editor(th, searchInput, "Input Query...")
-					e.Font.Style = text.Italic
-					border := widget.Border{Color: mainColor, CornerRadius: unit.Dp(8), Width: unit.Dp(2)}
-					return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-						return layout.UniformInset(unit.Dp(8)).Layout(gtx, e.Layout)
-					})
-				},
-				),
-				// layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-				// 		return material.List(theme, &list).Layout(gtx, len(messages), func(gtx layout.Context, i int) layout.Dimensions {
-				// 				m := material.Body1(theme, messages[i])
-				// 				m.Color = color.NRGBA{R: 0, G: 255, B: 0, A: 255}
-				// 				return m.Layout(gtx)
-				// 		})
-				// }),
-				// layout.Rigid(footer.Layout),
+				layout.Rigid(editor),
+				// layout.Rigid(header.Layout),
 			)
 
 			e.Frame(gtx.Ops)
